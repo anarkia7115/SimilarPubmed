73a7aee9c4a1d5ec8e70484994f86c4273b01f60
shawn <shawn24601@gmail.com>
2017-08-02 16:10:32 +0800

    change deploy shells

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,16 @@
 #scp /home/shawn/git/PubmedWordGame/SimilarPubmed/target/scala-2.11/SimilarPubmed-assembly-1.0.jar gcbi:~/jars/
-scp -r /home/shawn/git/PubmedWordGame/SimilarPubmed/src/main/python/* soldier1:/home/gcbi/workspace/calcScores
-scp -r /home/shawn/git/PubmedWordGame/SimilarPubmed/src/main/cpp/ soldier1:/home/gcbi/workspace/calcScores/src/main/
-scp /home/shawn/git/PubmedWordGame/SimilarPubmed/Makefile soldier1:/home/gcbi/workspace/calcScores/
+#scp -r /home/shawn/git/PubmedWordGame/SimilarPubmed/src/main/python/* soldier1:/home/gcbi/workspace/calcScores
+#scp -r /home/shawn/git/PubmedWordGame/SimilarPubmed/src/main/cpp/ soldier1:/home/gcbi/workspace/calcScores/src/main/
+#scp /home/shawn/git/PubmedWordGame/SimilarPubmed/Makefile soldier1:/home/gcbi/workspace/calcScores/
+
+#scp /home/shawn/git/PubmedWordGame/SimilarPubmed/target/scala-2.11/similarpubmed_2.11-1.0.jar cld:jars/
+#scp /home/shawn/git/PubmedWordGame/SimilarPubmed/calcScores_stream cld:bin/
+#scp /home/shawn/git/PubmedWordGame/SimilarPubmed/src/main/cpp/* cld:workspace/Similarpubmed/src/main/cpp
+
+scp /home/shawn/git/PubmedWordGame/SimilarPubmed/target/scala-2.11/similarpubmed_2.11-1.0.jar soldier1:jars/
+scp /home/shawn/git/PubmedWordGame/SimilarPubmed/src/main/cpp/* soldier1:workspace/Similarpubmed/src/main/cpp
+
+#scp /home/shawn/git/PubmedWordGame/SimilarPubmed/target/scala-2.11/similarpubmed_2.11-1.0.jar app04:/home/gcbi/SimilarPubmed/jars/
+#scp /home/shawn/git/PubmedWordGame/SimilarPubmed/target/scala-2.11/SimilarPubmed-assembly-1.0-deps.jar app04:/home/gcbi/SimilarPubmed/jars/
+#scp /home/shawn/git/PubmedWordGame/SimilarPubmed/src/main/cpp/* app04:/home/gcbi/SimilarPubmed/src/main/cpp
+#
