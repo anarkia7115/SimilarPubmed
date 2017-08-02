de2515e3216388328c4ade7df0f421a8f8bedf99
shawn <shawn24601@gmail.com>
2017-08-02 16:07:30 +0800

    add mutable.Map/Map overwrite

--- src/main/scala/algorithms/Weights.scala
+++ src/main/scala/algorithms/Weights.scala
@@ -31,6 +31,32 @@ class Weights(spark:SparkSession) extends java.io.Serializable {
     termScores
   }
 
+  def scoreMat(termFreqs:Map[String, Int], rowIdx:Int, idfs:Map[String, Double], termIds:Map[String, Int]):Seq[(Int, Int, Double)] = {
+
+    val eta = 1.0
+    val mu = 0.013
+    val lambda = 0.022
+    val absTotalTerms = termFreqs.values.sum
+    val l = absTotalTerms
+    val termSize = termIds.size
+
+    val termScores = termFreqs.filter{
+	case (term, freq) => termIds.contains(term)
+      }.map { case (term, freq) => {
+      val km1 = termFreqs(term).toDouble - eta
+      val idf = idfs(term)
+
+      val w = math.sqrt(idf) /
+	  (
+	    1.0 +
+	    math.pow( (mu / lambda), km1) *
+	    math.exp(  -(mu - lambda) * l)
+	  )
+      (termIds(term), rowIdx, w)
+    }}.toSeq
+
+    termScores
+  }
   def scoreMat(termFreqs:Map[String, Int], rowIdx:Int, idfs:Map[String, Double], termIds:immutable.Map[String, Int]):Seq[(Int, Int, Double)] = {
 
     val eta = 1.0
@@ -56,6 +82,23 @@ class Weights(spark:SparkSession) extends java.io.Serializable {
     }}.toSeq
 
     termScores
+  }
+
+  /*
+   * Input: [Int, Map[String, Int]]
+   * */
+  def scoreMatByDocs(
+    tfByDocs:DataFrame
+    , idfs:Map[String, Double]
+    , termIds:Map[String, Int]
+  ): DataFrame= {
+    val mat = tfByDocs.flatMap(attrs => {
+      val pmid = attrs.getInt(0)
+      val termFreqs = attrs.getMap[String, Int](1)
+      scoreMat(termFreqs, pmid, idfs, termIds)
+    }).toDF("term_id", "pmid", "weight")
+    return mat
+    //.toDF("term_id", "pmid", "score")
   }
 
   /*
